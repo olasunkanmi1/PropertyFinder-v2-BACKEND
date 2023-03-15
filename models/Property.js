4d2e9af3954a1a09d4190741f735a855ffa726ee
Olasunkanmi <abdulsalamquadri999@gmail.com>
2023-03-15 23:56:41 +0100

    update PropertySchema

--- models/Property.js
+++ models/Property.js
@@ -4,47 +4,15 @@ import bcrypt from "bcryptjs";
 
 const PropertySchema = new mongoose.Schema({
     coverPhoto: { type: String, required: true },
-    firstName: {
-        type: String,
-        required: [true, 'Please provide first name'],
-        minLength: 3,
-        maxLength: 20,
-        trim: true
-    },
-    lastName: {
-        type: String,
-        required: [true, 'Please provide last name'],
-        minLength: 3,
-        maxLength: 20,
-        trim: true
-    },
-    email: {
-        type: String,
-        unique: true,
-        required: [true, 'Please provide email'],
-        validate: {
-            validator: validator.isEmail,
-            message: 'Please provide a valid email'
-        }
-    },
-    password: {
-        type: String,
-        required: [true, 'Please provide password'],
-        minLength: 6,
-        Select: false
-    },
-    verificationToken: String,
-  isVerified: {
-    type: Boolean,
-    default: false,
-  },
-  verified: Date,
-  passwordToken: {
-    type: String,
-  },
-  passwordTokenExpirationDate: {
-    type: Date,
-  }
+    price: { type: String, required: true },
+    rooms: { type: String, required: true },
+    title: { type: String, required: true },
+    baths: { type: String, required: true },
+    area: { type: String, required: true },
+    isVerified: { type: Boolean, required: true },
+    rentFrequency: { type: String, required: true },
+    agency: { type: String, required: true },
+    externalID: { type: String, required: true },
 });
 
 export default mongoose.model('Property', PropertySchema)
